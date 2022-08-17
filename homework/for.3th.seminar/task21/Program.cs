e68434e18222ba7b63aac4866ff370d2a7c7a0f5
Anna.K <kkk.aanya1996@mail.ru>
2022-08-17 18:05:22 +0300

    task21

--- homework/for.3th.seminar/task21/Program.cs
+++ homework/for.3th.seminar/task21/Program.cs
@@ -3,3 +3,22 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
+
+Console.WriteLine("Введите X1: ");
+int userX1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите X2: ");
+int userX2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y1: ");
+int userY1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y2: ");
+int userY2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z1: ");
+int userZ1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z2: ");
+int userZ2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt((userX2-userX1)*(userX2-userX1)+(userY2-userY1)*(userY2-userY1)+(userZ2-userZ1)*(userZ2-userZ1));
+distance = Math.Round(distance, 2);
+Console.WriteLine($"Расстояние равно: {distance}");
+
+
--- homework/for.3th.seminar/task23/Program.cs
+++ homework/for.3th.seminar/task23/Program.cs
@@ -4,3 +4,18 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125 
 */
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("1");
+for(int i = 2; i<=userNumber; i++) 
+{
+    int square = i*i;
+    Console.Write($",{square}");
+}
+
+
+/* if i=userNumber;
+    {
+        Console.Write($"{square}");
+    } */
