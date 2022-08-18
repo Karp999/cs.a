4a8f6a7dd48bc43fc8740234eeacd50ace0c8251
Anna.K <kkk.aanya1996@mail.ru>
2022-08-18 18:12:36 +0300

    task19

--- homework/for.3th.seminar/task19/Program.cs
+++ homework/for.3th.seminar/task19/Program.cs
@@ -5,3 +5,19 @@
 12821 -> да
 23432 -> да
 */
+Console.WriteLine("Введите пятизначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = userNumber/10000; 
+int secondDigit = (userNumber/1000)%10; 
+int fourthDigit = (userNumber/10)%10; 
+int fifthDigit = userNumber%10; 
+
+if (firstDigit == fifthDigit | secondDigit == fourthDigit)
+{
+    Console.WriteLine("Это палиндром!");
+}
+else 
+{
+    Console.WriteLine("Это не палиндром!");
+}
