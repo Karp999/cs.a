8bbdafa8f5d0430ef4094c1d664891f65849f2f0
Anna.K <kkk.aanya1996@mail.ru>
2022-08-22 20:10:47 +0300

    two-dimensional array

---
+++ lections/lect4/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЛЕКЦИЯ №4. Двумерные массивы и рекурсия 
+
+string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+//обращение к нужному элементу массива: table[1,2]
+//создаём таблицу циклом в цикле,где rows-строки, columns-столбцы
+for (int rows=0; rows<2; rows++)
+{
+    for (int columns=0; columns<5; columns++)
+    {
+        Console.WriteLine($"-{table[rows,columns]}-");
+    }
+}
