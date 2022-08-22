67809c35f6930fcebfd8585208a4a14edfc03df9
Anna.K <kkk.aanya1996@mail.ru>
2022-08-22 18:17:16 +0300

    working with array.error

--- homework/for.4th.seminar/task29/Program.cs
+++ homework/for.4th.seminar/task29/Program.cs
@@ -3,48 +3,28 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
-
-int[] arr = new int[8];
-
-
-
-/*ПРИМЕРЫ:
-int[] arr = new int[8];
-
-for(int i=0; i < arr.Length; i++) {
-    arr[i] = new Random().Next(0,2);
-}
-
-for(int i=0; i < arr.Length; i++) {
-    Console.Write($"{arr[i]} ");
-}
-
-/*или варианты:
-
-void FillArray(int[] collection)
+void FillArray(int[] digits);
 {
-    int length = collection.Length;
-    int index = 0;
-    while(index < length)
+    int length = digits.Length;
+    int i = 0;
+    while (i<length)
     {
-        collection[index] = new Random().Next(0, 2);
-        index++;
+        digits[i] = new Random().Next(0,999);
+        i++;
     }
 }
 
-void PrintArray(int[] col)
+void PrintArray(int[] nums)
 {
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
+    int i = nums.Length;
+    int position = 0; //позиция элемента в массиве.Взяла на заметку из лекции,где решили заводить позицию не через индекс
+    while (position < i)
     {
-        Console.Write($"{col[position]} ");
+        Console.Write($"{nums[position]} ");
         position++;
     }
 
 }
-
-int [] array = new int[8];
-
+int[] array = new int[8];
 FillArray(array);
-PrintArray(array); */
+PrintArray(array);
