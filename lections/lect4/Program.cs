2ffcad15b9a50f00fdba724b09a607acdd3bf94a
Anna.K <kkk.aanya1996@mail.ru>
2022-08-22 21:42:27 +0300

    pic

--- lections/lect4/Program.cs
+++ lections/lect4/Program.cs
@@ -14,8 +14,7 @@ for (int rows=0; rows<2; rows++)
 }
 */
 
-//Пример массива из чисел
-
+/*Пример массива из чисел:
 int[,] matrix = new int[3,4];
 //Здесь Сергей сказал, что rows в числовом виде часто пишут i, columns - j
 for (int i=0; i<matrix.GetLength(0); i++)
@@ -25,7 +24,43 @@ for (int i=0; i<matrix.GetLength(0); i++)
     {
         Console.Write($"{matrix[i,j]}");
         /*здесь была ошибка:вывод в 1 столбец,так как Console.WriteLine($"{matrix[i,j]}")-мы каждый раз 
-        переходили на новую строку.теперь мы остаёмся на строке, а потом в основной цикл добавим пустую строку */
+        переходили на новую строку.теперь мы остаёмся на строке, а потом в основной цикл добавим пустую строку
     }
     Console.WriteLine();
 }
+*/
+
+//Метод, печатающий матрицу на экран и заполняющий её числами:
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+             Console.Write($"{matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); //[1;10)-полуинтервал,несмотря на то, что скобки круглые
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+
+Console.WriteLine();
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+
