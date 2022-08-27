b3803b05cbbd0b76f3e13748942c3d1eae0dde2c
Anna.K <kkk.aanya1996@mail.ru>
2022-08-27 18:09:09 +0300

    z40

--- seminars/sem6/z40/Program.cs
+++ seminars/sem6/z40/Program.cs
@@ -2,6 +2,35 @@
 существовать треугольник с сторонами такой длины.
 Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 */
+int a, b, c;
+Console.WriteLine("Введите длину стороны a: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину стороны b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину стороны c: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+string result;
+result = Result(a, b, c);
+Console.WriteLine(result);
+
+string Result (int a, int b, int c)
+{
+    string result = string.Empty;
+    if (a < (b + c) && b < (a + c) && c < (b + a))
+    {
+        result = "Такой треугольник имеет право на жизнь";
+    }
+    else
+    {
+        result = "Такой треугольник не существует";
+    }
+    return result;
+}
+
+/* ИЛИ
 
 Console.WriteLine("Введите длину первой стороны треугольника: ");
 int firstSide = Convert.ToInt32(Console.ReadLine());
@@ -20,3 +49,37 @@ else
 {
      Console.WriteLine($"Треугольника со сторонами {firstSide}, {secondSide}, {thirdSide} НЕ существует");
 }
+*/
+
+/* ИЛИ
+
+int userNumberOne = new int();
+int userNumberTwo = new int();
+int userNumberThree = new int();
+
+Console.WriteLine("Введите первое число: ");
+userNumberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число число: ");
+userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число число: ");
+userNumberThree = Convert.ToInt32(Console.ReadLine());
+
+void GetTriangle(int a, int b, int c)
+{
+
+if (userNumberOne < userNumberTwo + userNumberThree && userNumberTwo < userNumberOne + userNumberThree && userNumberThree < userNumberOne + userNumberTwo)
+{
+Console.Write("Треугольник может существовать!");
+}
+else
+{
+Console.Write("Не треугольник!");
+}
+}
+
+GetTriangle(userNumberOne, userNumberTwo, userNumberThree);
+
+
+*/
