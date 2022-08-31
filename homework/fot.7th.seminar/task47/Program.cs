43af6910cfb4bf61b2418d94feeddef51dd1486f
Anna.K <kkk.aanya1996@mail.ru>
2022-08-31 22:21:02 +0300

    tasks 50,52

--- homework/fot.7th.seminar/task47/Program.cs
+++ homework/fot.7th.seminar/task47/Program.cs
@@ -39,7 +39,8 @@ void PrintArray(double[,] array)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($" | { array[i,j] } | "); //поставила вертикальные линии, чтоб красивее было
+            Console.Write($" | { array[i,j] } | "); 
+            //поставила вертикальные линии в выводе, чтоб зрительно чётче разделение было и для красоты
         }
         Console.WriteLine();
     }
--- homework/fot.7th.seminar/task52/Program.cs
+++ homework/fot.7th.seminar/task52/Program.cs
@@ -5,3 +5,46 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+int[,] matrix = new int[3,4];
+/* int minValue = -10;
+int maxValue = 10; */
+for (int i=0; i<matrix.GetLength(0); i++)
+{
+    for (int j=0; j<matrix.GetLength(1); j++)
+    {
+        /* int result[i,j] = new Random().Next(minValue, maxValue); */
+        Console.Write($"{matrix[i,j]}");
+    }
+    Console.WriteLine();
+}
+
+
+/*
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" | {array[i, j]} | ");
+            //поставила вертикальные линии в выводе, чтоб зрительно чётче разделение было и для красоты
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ColumnAverage(int m, int n, int minValue, int maxValue)
+{
+    int[,] AverageArray = new int[m, n];
+
+    int AverageColumnA =
+    int AverageColumnB =
+    int AverageColumnC =
+    int AverageColumnD =
+
+    Console.Write($"{AverageColumnA}, {AverageColumnA}, {AverageColumnA}, {AverageColumnA}");
+}
+*/
