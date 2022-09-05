da427ca3a43a3e9724257c3cef96e985c822cf91
Anna.K <kkk.aanya1996@mail.ru>
2022-09-05 19:07:24 +0300

    corrected task 62

--- homework/for.8th.seminar/task62/Program.cs
+++ homework/for.8th.seminar/task62/Program.cs
@@ -5,3 +5,68 @@
 11 16 15 06
 10 09 08 07
 */
+
+/* взяла в основу комментарии с семинара и информацию из Интернета, 
+что помогло разобраться в заполнении спирали. */
+int[,] SpiralArray = new int[4, 4];
+Console.WriteLine();
+FillSpiralArray(SpiralArray);
+PrintArray(SpiralArray);
+Console.WriteLine();
+
+void FillSpiralArray (int[,] SpiralArray) //метод заполнения массива
+{
+    // точки начала и конца сторон квадрата первоначально все приравниваем к нулю
+    int movingFromA = 0; 
+    int movingToA = 0;
+    int movingFromB = 0;
+    int movingToB = 0;
+
+    int count = 1;
+    int i = 0;
+    int j = 0;
+    while (count <= SpiralArray.GetLength(0) * SpiralArray.GetLength(1))
+    {
+        SpiralArray[i, j] = count;
+        
+        if (i == movingFromA && j < SpiralArray.GetLength(1) - movingToB - 1) // движемся вправо
+        {
+            j++;
+        }
+        else if (j == SpiralArray.GetLength(1) - movingToB - 1 && i < SpiralArray.GetLength(0) - movingToA - 1)  // движемся вниз
+        {
+            i++;
+        }
+        else if (i == SpiralArray.GetLength(0) - movingToA - 1 && j > movingFromB) // движемся влево
+        {
+            j--;
+        }
+        else 
+        {
+            i--;
+        }
+
+        if ((i == movingFromA + 1) && (j == movingFromB) && (movingFromB != SpiralArray.GetLength(1) - movingToB - 1))
+        {
+            movingFromA++;
+            movingToA++;
+            movingFromB++;
+            movingToB++;
+        }
+        count++;
+    }
+}
+
+void PrintArray(int[,] array) //метод печати массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10) Console.Write($" 0{array[i, j]}"); /*добавила 0, чтоб однозначные 
+            числа начинались с нуля: 01,02,03... */
+            else Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
