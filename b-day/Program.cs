f7f66f8427aca409fd6aa75efdaa94c70fbfd1b5
Anna.K <kkk.aanya1996@mail.ru>
2022-09-05 17:05:51 +0300

    corrected task 58

---
+++ b-day/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача: высчитать свой возраст по дате рождения
+int GetAge(DateTime birthDate)
+{
+    var now = DateTime.Today;
+    return now.Year - birthDate.Year - 1 +
+        ((now.Month > birthDate.Month || now.Month == birthDate.Month && now.Day >= birthDate.Day) ? 1 : 0);
+}
+
+var birthDate = 09.09.96,
+GetAge(birthDate);
+*/
--- homework/for.8th.seminar/task58/Program.cs
+++ homework/for.8th.seminar/task58/Program.cs
@@ -7,58 +7,93 @@
 15 18
 */
 
-
-int[,] firstArray = new int[4, 4];
-int[,] secondArray = new int[4, 4];
-//для умножения матриц количество столбцов и строк матриц должно совпадать, поэтому создаю две матрицы 4*4.
-Console.WriteLine();
-FillArray(firstArray,secondArray);
-PrintArray(firstArray,secondArray);
+int[,] firstMatrix = new int[2, 2];
+int[,] secondMatrix = new int[2, 2];
+/* Для умножения матриц количество столбцов и строк матриц должно совпадать, 
+поэтому создаю две матрицы 2*2, как в примере. */
 Console.WriteLine();
 
+FillMatrix(firstMatrix, secondMatrix);
+PrintMatrix(firstMatrix, secondMatrix);
+Console.WriteLine();
 
-void FillArray(int[,] firstArray, int[,] secondArray)
+int[,] CommonMatrix = MatrixMultiplication(firstMatrix, secondMatrix);
+Console.WriteLine("Произведение двух матриц: ");
+Console.WriteLine();
+PrintCommonMatrix(CommonMatrix);
+Console.WriteLine();
+
+void FillMatrix(int[,] firstMatrix, int[,] secondMatrix)
 {
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 2; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < 2; j++)
         {
-            firstArray[i, j] = new Random().Next(0, 10);
+            firstMatrix[i, j] = new Random().Next(0, 7);
         }
     }
-    for (int k = 0; k < 4; k++)
+    for (int i = 0; i < 2; i++)
     {
-        for (int v = 0; v < 4; v++)
+        for (int j = 0; j < 2; j++)
         {
-            secondArray[k, v] = new Random().Next(0, 10);
+            secondMatrix[i, j] = new Random().Next(0, 7);
         }
     }
 }
 
-void PrintArray(int[,] firstArray, int[,] secondArray)
+void PrintMatrix(int[,] firstMatrix, int[,] secondMatrix)
 {
-    for (int i = 0; i < firstArray.GetLength(0); i++)
+    Console.WriteLine(" Первая матрица:");
+    Console.WriteLine();
+    for (int i = 0; i < firstMatrix.GetLength(0); i++)
     {
-        for (int j = 0; j < firstArray.GetLength(1); j++)
+        for (int j = 0; j < firstMatrix.GetLength(1); j++)
         {
-            Console.Write($" | {firstArray[i, j]} | "); //вертикальные знаки для чёткости и красоты вывода
+            Console.Write($" | {firstMatrix[i, j]} | "); //вертикальные знаки для чёткости и красоты вывода
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 
-    for (int k = 0; k < secondArray.GetLength(0); k++)
+    Console.WriteLine(" Вторая матрица:");
+    Console.WriteLine();
+    for (int i = 0; i < secondMatrix.GetLength(0); i++)
     {
-        for (int v = 0; v < secondArray.GetLength(1); v++)
+        for (int j = 0; j < secondMatrix.GetLength(1); j++)
         {
-            Console.Write($" | {secondArray[k, v]} | "); //вертикальные знаки для чёткости и красоты вывода
+
+            Console.Write($" | {secondMatrix[i, j]} | "); //вертикальные знаки для чёткости и красоты вывода
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-void ArrayMultiplication (int[,] firstArray, int[,] secondArray)
+int[,] MatrixMultiplication(int[,] firstMatrix, int[,] secondMatrix)
 {
-    
+    int[,] resultMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            for (int v = 0; v < firstMatrix.GetLength(1); v++)
+            {
+                resultMatrix[i, j] = resultMatrix[i, j] + firstMatrix[i, v] * secondMatrix[v, j];
+            }
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintCommonMatrix(int[,] MatrixMultiplication)
+{
+    for (int i = 0; i < MatrixMultiplication.GetLength(0); i++)
+    {
+        for (int j = 0; j < MatrixMultiplication.GetLength(1); j++)
+        {
+            Console.Write($" | {MatrixMultiplication[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
 }
