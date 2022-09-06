c35fe2231dd65f2e67f946c02c453f0ab8a0dbef
Anna.K <kkk.aanya1996@mail.ru>
2022-09-06 20:58:50 +0300

    task 68

--- homework/for.9th.seminar/task66/Program.cs
+++ homework/for.9th.seminar/task66/Program.cs
@@ -4,3 +4,29 @@
 M = 1; N = 15 -> 56
 M = 4; N = 8. -> 18
 */
+
+Console.WriteLine();
+Console.Write("Введите число M: ");
+int userM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int userN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int SumOfEvenNumbers(int userM, int userN)
+{
+    if (userM <= userN)
+    {
+        if (userM % 2 == 0)
+        {
+            return userM + SumOfEvenNumbers(userM + 2, userN);
+        }
+        else
+        {
+            userM = userM + 1;
+            return userM + SumOfEvenNumbers(userM + 2, userN);
+        }
+    }
+    else return 0;
+}
+Console.WriteLine($"Сумма чётных чисел в промежутке от {userM} до {userN} равна: {SumOfEvenNumbers(userM, userN)} . ");
+Console.WriteLine();
