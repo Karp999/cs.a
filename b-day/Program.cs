b01b67619d8efe1b136c490bfb68ece6d1f449dc
Anna.K <kkk.aanya1996@mail.ru>
2022-09-09 12:41:04 +0300

    my b-day

--- b-day/Program.cs
+++ b-day/Program.cs
@@ -1,11 +1,21 @@
-﻿/* Задача: высчитать свой возраст по дате рождения
-int GetAge(DateTime birthDate)
-{
-    var now = DateTime.Today;
-    return now.Year - birthDate.Year - 1 +
-        ((now.Month > birthDate.Month || now.Month == birthDate.Month && now.Day >= birthDate.Day) ? 1 : 0);
-}
+﻿Console.Clear();
 
-var birthDate = 09.09.96,
-GetAge(birthDate);
+DateTime myDateOfBirth = new DateTime(1996, 9, 9); 
+Console.WriteLine($"Date of birthday: {myDateOfBirth.ToShortDateString()}");
+DateTime todayBday = new DateTime(2022, 9, 9); 
+Console.WriteLine($"Today: {todayBday.ToShortDateString()}");
+Console.WriteLine($"Hello,{(todayBday.Subtract(myDateOfBirth))/360} Anya's years!♥ ♥ ♥ ");
+
+/*
+int CalculateAge(DateTime birthDate)
+{todayBday.Subtract(myDateOfBirth)
+    DateTime birthDate = new DateTime(1996, 9, 9);
+    DateTime today = DateTime.Today;
+    int age = today.Year - birthDate.Year;
+    if (birthDate.AddYears(age) > today)
+    {
+        age--;
+    }
+    return age;
+}
 */
